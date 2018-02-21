321a69ff01e9782ceba24ec4428532aca1cf5c9a
Camacho <abcamacho.2017@mymail.jcsu.edu>
2018-02-21 14:31:44 -0500

    first commit

---
+++ src/Person.java
@@ -0,0 +1,16 @@
+
+public class Person {
+String name;
+int age;
+public Person(String name2, int age2){
+	name = name2;
+	age = age2;
+}
+public boolean isAdult(){
+	if(age>18){
+		return true;
+	}else{
+		return false;
+	}
+}
+}
---
+++ src/PersonTest.java
@@ -0,0 +1,11 @@
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Person pl = new Person("Joy", 17);
+System.out.println(pl.isAdult());
+
+	}
+
+}
